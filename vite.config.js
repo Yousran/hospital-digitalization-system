14f5f65596673f2b092a667ececa98cf6b7064fd
Yousran <yusranmazidan@gmail.com>
2024-11-11 09:47:40 +0800

    FEAT: Docker Init

--- vite.config.js
+++ vite.config.js
@@ -8,4 +8,14 @@ export default defineConfig({
             refresh: true,
         }),
     ],
+    server: {
+        host: '0.0.0.0',
+        port: 5173,
+        hmr: {
+            host: 'localhost', // Hot Module Replacement setup
+        },
+    },
 });
+//TODO:dikarenakan vite yang berjalan sangat lambat jika dijalankan melalui docker container
+//maka jalankan 'npm run dev' di terminal yang berada di luar container
+//pastikan sudah menginstall npm dan node js lalu menjalankan perintah npm install untuk menginstal vite nya
