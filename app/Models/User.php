6623b1941027bacbb44e5b57e83571227595ae5d
Yousran <yusranmazidan@gmail.com>
2024-11-12 02:45:23 +0000

    FIX: User Relation

--- app/Models/User.php
+++ app/Models/User.php
@@ -32,7 +32,7 @@ class User extends Authenticatable
     }
 
     public function roles(){
-        return $this->belongsToMany(Role::class)->withPivot('user_id', 'role_id');
+        return $this->belongsToMany(Role::class, 'user_role')->withPivot('user_id', 'role_id');
     }
 
     public function doctor(){
