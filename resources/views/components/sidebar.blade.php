d89b5d2e9cbbbfc3d2fb0b0eeb0370fa49ab763d
Yousran <yusranmazidan@gmail.com>
2024-11-17 13:41:35 +0000

    FEAT: Logs

--- resources/views/components/sidebar.blade.php
+++ resources/views/components/sidebar.blade.php
@@ -94,7 +94,7 @@
                 </a>
             </li>
             <li>
-                <a href="#"
+                <a href="{{ route('log-viewer.index') }}"
                     class="flex items-center p-2 text-dark-300 rounded-lg dark:text-light-800 hover:bg-light-500 hover:text-dark-500 dark:hover:text-light-500 dark:hover:bg-dark-500 group">
                     <i
                         class='bx bx-history flex-shrink-0 w-5 h-5 text-2xl text-dark-300 transition duration-75 dark:text-light-800 group-hover:text-dark-500 dark:group-hover:text-light-500'></i>
