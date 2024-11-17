bdef307909e67a34d3184e28da679c00d6589c0a
Yousran <yusranmazidan@gmail.com>
2024-11-17 06:48:38 +0000

    FIX: Login and Register Styles

--- resources/views/pages/login.blade.php
+++ resources/views/pages/login.blade.php
@@ -1,66 +1,47 @@
 @extends('layouts.form')
 @section('contents')
-    <section class="bg-gray-50 dark:bg-gray-900 h-full">
-        <div class="flex flex-col items-center justify-center px-6 py-8 mx-auto h-screen">
-            <a href="{{ route('home') }}" class="flex items-center mb-6 text-2xl font-semibold text-gray-900 dark:text-white">
-                <img src="logo.png" class="w-8 h-8 mr-4" alt="Logo" />
-                Rumah Sehat
-            </a>
-            <div
-                class="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
-                <div class="p-6 space-y-4 md:space-y-6 sm:p-8">
-                    <h1 class="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
-                        Login to your account
-                    </h1>
-                    <form class="space-y-4 md:space-y-6" action="{{ route('login') }}" method="POST">
-                        @csrf
-                        <div>
-                            <label for="email" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your
-                                email</label>
-                            <input type="email" name="email" id="email"
-                                class="bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                                placeholder="name@company.com" required="">
-                        </div>
-                        <div>
-                            <label for="password"
-                                class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Password</label>
-                            <input type="password" name="password" id="password" placeholder="••••••••"
-                                class="bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                                required="">
-                        </div>
-                        <div class="flex items-center justify-between">
-                            <div class="flex items-start">
-                                <div class="flex items-center h-5">
-                                    <input id="remember" aria-describedby="remember" type="checkbox"
-                                        class="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-primary-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-primary-600 dark:ring-offset-gray-800">
-                                </div>
-                                <div class="ml-3 text-sm">
-                                    <label for="remember" class="text-gray-500 dark:text-gray-300">Remember me</label>
-                                </div>
-                            </div>
-                            <a href="#"
-                                class="text-sm font-medium text-primary-600 hover:underline dark:text-primary-500">Forgot
-                                password?</a>
-                        </div>
-                        <button type="submit"
-                            class="w-full text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">Login</button>
-                        <button type="button"
-                            class="w-full flex items-center justify-center text-gray-900 bg-white border border-gray-300 hover:bg-gray-100 focus:ring-4 focus:outline-none focus:ring-gray-200 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-gray-700 dark:text-white dark:border-gray-600 dark:hover:bg-gray-600 dark:focus:ring-gray-700">
-                            <svg class="w-5 h-5 mr-2" aria-hidden="true" focusable="false" data-prefix="fab"
-                                data-icon="google" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 488 512">
-                                <path fill="currentColor"
-                                    d="M488 261.8c0-17.7-1.6-35.2-4.7-52H249v98.4h134.6c-8.2 41.9-33.1 77.4-70.6 101.5v83.6h113.7c66.4-61.2 104.3-151.4 104.3-231.5zM249 512c67.2 0 123.6-22.4 164.8-60.7l-113.7-83.6c-20.8 14-47.3 22.4-76.4 22.4-58.8 0-108.6-39.8-126.4-93.2H9.2v85.2C44.6 454.6 141.1 512 249 512zM122.6 306.9c-5.2-15.5-8.1-32.1-8.1-49s2.9-33.5 8.1-49v-85.2H9.2C-6.7 206.5-14 247.5-14 289.8s7.3 83.3 23.2 116.2l113.7-83.6zM249 114.6c29.1 0 55.6 10.1 76.4 22.4l113.7-83.6C372.6 22.4 316.2 0 249 0 141.1 0 44.6 57.4 9.2 127.8l113.7 83.6c17.8-53.4 67.6-93.2 126.4-93.2z">
-                                </path>
-                            </svg>
-                            Login with Google
-                        </button>
-                        <p class="text-sm font-light text-gray-500 dark:text-gray-400">
-                            Don't have an account yet? <a href="{{ route('register') }}"
-                                class="font-medium text-primary-600 hover:underline dark:text-primary-500">Register</a>
-                        </p>
-                    </form>
+    <h1 class="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
+        Login to your account
+    </h1>
+    <form class="space-y-4 md:space-y-6" action="{{ route('login') }}" method="POST">
+        @csrf
+        <div>
+            <label for="email" class="block mb-2 text-sm font-medium text-dark-500 dark:text-light-500">Email User</label>
+            <input type="email" name="email" id="email" class="w-full p-2.5 bg-light-500 border border-light-700 text-dark-500 rounded-lg focus:ring-primary-500 focus:border-primary-500 dark:bg-dark-300 dark:border-dark-100 dark:placeholder-dark-100 dark:text-light-500 dark:focus:ring-primary-500 dark:focus:border-primary-500" placeholder="name@company.com" required="">
+        </div>
+        <div>
+            <label for="password" class="block mb-2 text-sm font-medium text-dark-500 dark:text-light-500">Password</label>
+            <input type="password" name="password" id="password" placeholder="••••••••"
+                class="w-full p-2.5 bg-light-500 border border-light-700 text-dark-500 rounded-lg focus:ring-primary-500 focus:border-primary-500 dark:bg-dark-300 dark:border-dark-100 dark:placeholder-dark-100 dark:text-light-500 dark:focus:ring-primary-500 dark:focus:border-primary-500" required="">
+        </div>
+        <div class="flex items-center justify-between">
+            <div class="flex items-start">
+                <div class="flex items-center h-5">
+                    <input id="remember" aria-describedby="remember" type="checkbox"
+                        class="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-primary-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-primary-600 dark:ring-offset-gray-800">
+                </div>
+                <div class="ml-3 text-sm">
+                    <label for="remember" class="text-gray-500 dark:text-gray-300">Remember me</label>
                 </div>
             </div>
+            <a href="#"
+                class="text-sm font-medium text-primary-600 hover:underline dark:text-primary-500">Forgot password?</a>
         </div>
-    </section>
+        <button type="submit"
+            class="w-full text-white bg-primary-500 hover:bg-primary-600 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:focus:ring-primary-800">Login</button>
+        <button type="button"
+            class="w-full flex items-center justify-center 
+            text-dark-300 bg-white border border-gray-300 hover:bg-gray-100 
+            focus:ring-4 focus:outline-none 
+            focus:ring-gray-200 font-medium rounded-lg text-sm px-5 py-2.5 text-center
+            dark:bg-dark-300 dark:text-white dark:border-dark-200
+            dark:hover:bg-dark-200 dark:focus:ring-gray-700">
+            <i class="bx bxl-google w-5 h-5 text-2xl me-4"></i>
+            Login with Google
+        </button>
+        <p class="text-sm font-light text-dark-300 dark:text-light-600">
+            Don't have an account yet? <a href="{{ route('register') }}"
+                class="font-medium text-primary-500 hover:underline dark:text-light-500">Register</a>
+        </p>
+    </form>
 @endsection
